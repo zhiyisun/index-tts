35b6514ee5f96be6c1b45322fdc2dabfe2e2e879
Yrom <yrom@qq.com>
2025-04-24 20:10:26 +0800

    Enhance text normalization and tokenization
    - Introduced `de_tokenized_by_CJK_char` for restoring original text from tokenized format.
    - Added `TextTokenizer` class for improved tokenization, including sentence splitting and handling of special tokens.
    - Enhanced `TextNormalizer` to handle names and pinyin tones with placeholder mechanisms.
    - Added regression tests for new features in `regression_test.py`.

--- tests/regression_test.py
+++ tests/regression_test.py
@@ -10,7 +10,14 @@ if __name__ == "__main__":
     tts.infer(audio_prompt=prompt_wav, text=text, output_path=f"outputs/{text[:20]}.wav", verbose=True)
     text="There is a vehicle arriving in dock number 7?"
     tts.infer(audio_prompt=prompt_wav, text=text, output_path=f"outputs/{text[:20]}.wav", verbose=True)
-
+    text = "“我爱你！”的英语是“I love you!”"
+    tts.infer(audio_prompt=prompt_wav, text=text, output_path=f"outputs/{text[:20]}.wav", verbose=True)
+    text = "Joseph Gordon-Levitt is an American actor"
+    tts.infer(audio_prompt=prompt_wav, text=text, output_path=f"outputs/{text[:20]}.wav", verbose=True)
+    text = "约瑟夫·高登-莱维特是美国演员"
+    tts.infer(audio_prompt=prompt_wav, text=text, output_path=f"outputs/{text[:20]}.wav", verbose=True)
+    text = "蒂莫西·唐纳德·库克（英文名：Timothy Donald Cook），通称蒂姆·库克（Tim Cook），现任苹果公司首席执行官。"
+    tts.infer(audio_prompt=prompt_wav, text=text, output_path="outputs/蒂莫西·唐纳德·库克.wav", verbose=True)
     # 并行推理测试
     text="亲爱的伙伴们，大家好！每一次的努力都是为了更好的未来，要善于从失败中汲取经验，让我们一起勇敢前行,迈向更加美好的明天！"
     tts.infer_fast(audio_prompt=prompt_wav, text=text, output_path=f"outputs/{text[:20]}.wav", verbose=True)
@@ -25,4 +32,11 @@ if __name__ == "__main__":
 感谢您的收听，下期再见！
     '''.replace("\n", "")
     tts.infer_fast(audio_prompt=prompt_wav, text=text, output_path=f"outputs/{text[:20]}.wav", verbose=True)
-
+    # 长文本推理测试
+    text = """《盗梦空间》是由美国华纳兄弟影片公司出品的电影，由克里斯托弗·诺兰执导并编剧，
+莱昂纳多·迪卡普里奥、玛丽昂·歌迪亚、约瑟夫·高登-莱维特、艾利奥特·佩吉、汤姆·哈迪等联袂主演，
+2010年7月16日在美国上映，2010年9月1日在中国内地上映，2020年8月28日在中国内地重映。
+影片剧情游走于梦境与现实之间，被定义为“发生在意识结构内的当代动作科幻片”，
+讲述了由莱昂纳多·迪卡普里奥扮演的造梦师，带领特工团队进入他人梦境，从他人的潜意识中盗取机密，并重塑他人梦境的故事。
+""".replace("\n", "")
+    tts.infer_fast(audio_prompt=prompt_wav, text=text, output_path=f"outputs/{text[:20]}.wav", verbose=True)
